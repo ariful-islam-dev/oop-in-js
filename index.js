b03e654c54be1de4b31bd49d934b8194bd2f9fd4
ariful4082 <ariful4082@gmail.com>
2023-07-11 07:49:19 +0600

    create mathod to update the property of class

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ class Person {
         this.name = name
         this.email = email
     }
+
+    changeName(name){
+        this.name = name
+    }
+  
+
+    sendEmail(msg){
+        console.log('to log(...data: any[]')
+        console.log("Sending email", msg)
+    }
+
+    print(){
+        console.log(this)
+    }
 }
 
 
 const p1 = new Person("Ariful Islam", "ariful@gmail")
 const p2 = new Person("Abdullah Al Zarif", "zarif@gmail.com")
 const p3 = new Person("Assadullah Al Labib", "labib@gmail.com")
-console.log(p1)
-console.log(p2)
-console.log(p3)
+
+// p1.print()
+p1.changeName('Ariful Islam Raju')
+p3.changeName('Labib')
+p3.print()
