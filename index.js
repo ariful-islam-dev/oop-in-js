586051c39f72d02188190b8740a90d279aebff6c
ariful4082 <ariful4082@gmail.com>
2023-07-13 00:25:29 +0600

    static method

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ person.email="arifulislam@gmail.com"
 
 class Person {
 
+    static className = 'PERSON';
+    static PI=3.1415;
+
     constructor(name, email){
         this._name = name
         this._email = email
@@ -20,18 +23,52 @@ class Person {
         if(value.toLowerCase() === 'stack learner') return;
         this._name = value
     }
+    get email(){
+        return this._email.toLowerCase()
+    }
+
     
 
     print(){
-        console.log(this)
+        console.log(this + '')
+    }
+
+    toString(){
+        return ` Name: ${this._name}, email: ${this._email}`
+    }
+
+   static isValid(age){
+        return age >=  18
+    }
+
+    static equal(p1,p2){
+        if(p1.name !== p2.name) return false;
+
+        if(p1.email !== p2.email) return false;
+
+        return true;
     }
 }
 
 
 const p1 = new Person("Ariful Islam", "ariful@gmail")
 const p2 = new Person("Abdullah Al Zarif", "zarif@gmail.com")
-const p3 = new Person("Assadullah Al Labib", "labib@gmail.com")
+const p3 = new Person("Assadullah Al Labib", "labib@gmail.com");
+const p6 = new Person("Assadullah Al Labib", "labib@gmail.com");
 
-p1.name = 'Stack Learner'
-p1.print()
+const p4 = {_name: "Test", _email: "test@gmail.com"};
 
+const testUser = {
+    name: "Test", 
+    email: "test@gmail.com",
+    age:25
+};
+
+
+let p5 = null;
+if(Person.isValid(testUser.age)){
+    p5 = new Person(testUser.name, testUser.email);
+};
+console.log(p5)
+
+console.log(Person.equal(p6, p3 )) 
